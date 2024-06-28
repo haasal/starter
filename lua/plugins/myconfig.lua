6e52ddf3c4b34de91254a54dea9da986a86a6240
Alexander Haas <mail@haas-alexander.com>
2024-06-28 12:21:18 +0200

    added default extras

---
+++ lua/plugins/myconfig.lua
@@ -0,0 +1,22 @@
+return {
+  -- Load default languages (minimal)
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+
+  -- Load some extras
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.leap" },
+  { import = "lazyvim.plugins.extras.editor.fzf" },
+  { import = "lazyvim.plugins.extras.util.dot" },
+
+  -- vscode support
+  { import = "lazyvim.plugins.extras.vscode" },
+
+  -- formatters
+  { import = "lazyvim.plugins.extras.formatting.black" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+}
